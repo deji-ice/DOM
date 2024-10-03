4f0b96262d4e78733d0031452d0cfaf84b303aa0
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-10-03 20:21:01 +0100

    Add functionality to toggle an image on button click

---
+++ sacra/sacra.js
@@ -0,0 +1,37 @@
+// const outputText = document.getElementById("output");
+// const groupTexts = document.getElementsByClassName("group");
+// const group = document.querySelectorAll(".group");
+// const imgElement = document.querySelector("img");
+
+// const secondParagraph = groupTexts[1];
+// secondParagraph.textContent = `I'm the second paragraph!`;
+
+// const outputId = outputText.getAttribute("class");
+// outputText.setAttribute("class", "yes");
+
+// imgElement.setAttribute(
+//   "src",
+//   "https://images.unsplash.com/photo-1727709350469-5fde916217a8?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8"
+// );
+
+const btnElement = document.getElementById("btn");
+console.log(btnElement);
+
+let imgElement;
+btnElement.addEventListener("click", function () {
+  if (!imgElement) {
+    imgElement = document.createElement("img");
+    imgElement.setAttribute(
+      "src",
+      "https://images.unsplash.com/photo-1727709350469-5fde916217a8?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8"
+    );
+    imgElement.style.height = "20rem";
+    document.body.appendChild(imgElement);
+  
+  } else if (imgElement.src) {
+    imgElement.src = "";
+    imgElement = null
+  }
+  btnElement.setAttribute("disabled", true);
+  console.log("button clicked");
+});
